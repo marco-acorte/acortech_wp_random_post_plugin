54fe8e7d747156e76e280cbb59cf631409468d5b
Marco Acorte <macorte@gmail.com>
2017-05-10 15:32:30 +0200

    first commit

---
+++ random_post.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Random Post by Acortech
+Plugin URI: https://www.acortech.it/products/wordpress/acortech-random-post
+Description: Acortech Random Post allows you to get a redirect 307 url to a random post.
+Author: Acor3
+Version: 1.0
+Author URI: https://www.acortech.it
+Author License: GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+// based on article "WPBeginner� Blog� Tutorials How to Redirect Users to a Random Post in WordPress"
+// http://www.wpbeginner.com/wp-tutorials/how-to-redirect-users-to-a-random-post-in-wordpress/
+
+
+add_action('init','random_add_rewrite');
+function random_add_rewrite() {
+       global $wp;
+       $wp->add_query_var('random');
+       add_rewrite_rule('random/?$', 'index.php?random=1', 'top');
+}
+
+add_action('template_redirect','random_template');
+function random_template() {
+       if (get_query_var('random') == 1) {
+               $posts = get_posts('post_type=post&orderby=rand&numberposts=1');
+               foreach($posts as $post) {
+                       $link = get_permalink($post);
+               }
+               wp_redirect($link,307);
+               exit;
+       }
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Random Post ===  
+Contributors: Acortech  
+Donate link: https://www.acortech.it/products/wordpress/acortech-random-post
+Tags: random, random post, random posts 
+Requires at least: 3.0  
+Tested up to: 4.7 
+Stable tag: 1.0.0
+License: GPLv3 or later  
+License URI: http://www.gnu.org/licenses/gpl-3.0.html 
+  
+Acortech Random Post plugin allows you to get a redirect 307 url to a random post.  
+  
+== Description ==  
+Acortech Random Post allows you to get a redirect 307 url to a random post.
+
+ex: https://www.acor3.it/?random=1
+
+
+based on Article "WPBeginner» Blog >> Tutorials How to Redirect Users to a Random Post in WordPress"
+http://www.wpbeginner.com/wp-tutorials/how-to-redirect-users-to-a-random-post-in-wordpress/
